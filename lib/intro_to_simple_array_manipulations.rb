f8208df3fe85d244374e514894432f1ba5b0b408
Matine temory <temorymatine@gmail.com>
2020-05-16 16:04:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,15 +2,15 @@ def using_push(array,string)
   array.push(string)
 end
 
-def using_shift(array,string)
-  array.unshift
+def using_unshift(array,string)
+  array.unshift(string)
 end
 
 def using_pop(array)
   array.pop
 end
 
-def pops_with_args(array)
+def pop_with_args(array)
   array.pop(2)
 end
 
